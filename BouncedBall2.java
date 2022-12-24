c71d867f56b33820cc7c33f5cdd073383c976469
NayeraAyman20 <120630150+NayeraAyman20@users.noreply.github.com>
2022-12-24 13:27:41 +0200

    Update BouncedBall2.java

--- BouncedBall2.java
+++ BouncedBall2.java
@@ -109,6 +109,32 @@ public class BouncedBall2 extends AnimListener implements GLEventListener, KeyLi
 
         gl.glDisable(GL.GL_BLEND);
     }
+  public void DrawRoller(GL gl, int x, int y, int index, float scale, int dir) {
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[32]);	// Turn Blending On
+        int angle = 0;
+
+        gl.glPushMatrix();
+        // gl.glTranslated(x , y , 0);
+        gl.glTranslated(x / (maxWidth / 2.0) - 0.9, y / (maxHeight / 2.0) - 0.9, 0);
+        gl.glScaled(0.25 * scale, 0.25 * scale, 1);
+        gl.glRotated(angle, 0, 0, 1);
+        //System.out.println(x +" " + y);
+        gl.glBegin(GL.GL_QUADS);
+        // Front Face
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
+    }
   
   public void DrawBackground(GL gl) {
 

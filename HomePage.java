aa25cb51e7e205e854e721bb2b1af932c0a29b7f
MiraMarkus <121019848+MiraMarkus@users.noreply.github.com>
2022-12-26 23:59:36 +0200

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -14,8 +14,18 @@ public class HomePage extends JFrame  {
    f.setSize(1100, 700);
    f.setDefaultCloseOperation(EXIT_ON_CLOSE);
    f.setLocationRelativeTo(null);
- 
-  
+   
+   ImageIcon icon = new ImageIcon("C:\\Users\\Administrator\\Desktop\\BackGround2.png");
+        JLabel label = new JLabel(icon);
+        label.setLayout(new FlowLayout(-300, 500, 0));
+        JScrollPane scrollpane = new JScrollPane(label);
+        scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        f.add(scrollpane);
+        f.setVisible(true);
+        JViewport viewport = scrollpane.getViewport();
+   
+   
  }
  public static void main(String[] args) {
         HomePage HP = new HomePage();

d80b825ffd218879199f221544635db7c91c0859
MiraMarkus <121019848+MiraMarkus@users.noreply.github.com>
2022-12-26 23:51:05 +0200

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -11,27 +11,7 @@ public class HomePage extends JFrame  {
   
  public HomePage(){
   
- this.setTitle("HomePage");
-        this.setVisible(true);
-        this.setSize(1100, 700);
-        this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setLayout(new FlowLayout(550, 550, 150));  
-        
-        B1=new JButton("    Start   ");
-        B1.addActionListener(new ActionListener() {
-     @Override
-     public void actionPerformed(ActionEvent ae) {
-         new BouncedBall();
-     }
- });
-        add(B1); 
-   B2=new JButton("How To Play");
-    
-   add(B2);
-   
- } 
+ 
   
   
  public static void main(String[] args) {
